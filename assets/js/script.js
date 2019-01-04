cddbdba1f748ab27a57f3dd4be92edd52961d4cf
Brian Yin <brianyin@berkeley.edu>
2019-01-03 22:49:54 -0800

    add eyes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    /*$(document).mousemove(function(event) {
+    $(document).mousemove(function(event) {
         let xPos = event.pageX;
         let yPos = event.pageY;
-        $("#bigcircle").css({left: 820-(xPos/70), position:'absolute'});
-        $("#smallcircle").css({left: 740-(xPos/70), position:'absolute'});
-    });*/
+
+        console.log(xPos);
+        $("#lefteye").css({right: 40 - (xPos/200), top: 16 + (yPos/200) - 2, position:'absolute'});
+        $("#righteye").css({right: 26 - (xPos/200), top: 16 + (yPos/200) - 2, position:'absolute'});
+    });
 
 });
