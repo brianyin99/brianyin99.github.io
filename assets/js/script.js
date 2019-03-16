08ecff3614b5e1413c6534d96871b005d2d71a6f
Brian Yin <brianyin@berkeley.edu>
2019-03-16 11:15:39 -0700

    scale project titles with browser width

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
 
     $(window).resize(function() {
       windowWidth = $(window).width();
-      //console.log(windowWidth)
+      console.log(windowWidth);
       if (windowWidth < 1000) {
-        $("#campanile").css({opacity: windowWidth * 0.001})
+        $("#campanile").css({opacity: windowWidth * 0.001});
       }
+
+      fontSize = Math.min(40, windowWidth / 18);
+      $("h1").css({'font-size': fontSize});
+
+
     })
 
 
